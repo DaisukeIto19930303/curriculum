99bd4bc3a0b50e78886cf902773ad9a74f175a4f
DaisukeIto19930303 <105031221+DaisukeIto19930303@users.noreply.github.com>
2022-12-25 23:21:52 +0900

    3-3(再提出)

--- htdocs/LetsEngineer/curriculum/3-3/bugTest/test2/dbInfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DB_DATABASE', 'YIGroupBlog');
-// MySQLのユーザー名
-define('DB_USERNAME', 'root');
-// MySQLのログインパスワード
-define('DB_PASSWORD', 'root');
-// DSN
-define('PDO_DSN', 'mysql:host=localhost;charset=utf8;dbname='.DB_DATABASE);
-?>
